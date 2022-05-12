af4436fc238beb1adafaf60ad27f9edf08bd7503
dvoritK <101967699+dvoritK@users.noreply.github.com>
2022-05-12 14:08:38 +0300

    Added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     return alert(`${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function newKeyword(event) {
     setKeyword(event.target.value);
   }
